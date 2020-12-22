826b42978e1fd0c187831360be4442b9faa827cd
msctrl <xan+stjj@kokosema.net>
2020-12-22 21:18:13 +0900

    クエリ修正

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import { search } from "api-service";
 export class App {
-  public message: string = 'Hello World!';
+  public message: string = 'Hello World!2';
   public result : string = "";
   public doSearch() {
     const query = {
-      "cardType": "Object",
-      "$or" : [
-        {"objectClass":"Neutralized"},
-        {"objectClass":"Explained"},
+      cardType: "Object",
+      $or : [
+        {objectClass:"Neutralized"},
+        {objectClass:"Explained"},
       ]
     }
     search(query).then(json => this.result = JSON.stringify(json));
