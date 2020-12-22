7f16a884adfd9bce529303c7b007347649ec7736
msctrl <xan+stjj@kokosema.net>
2020-12-22 22:53:43 +0900

    変更が分かるように変更

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { search } from "api-service";
 import { Card } from "card";
 export class App {
-  public message: string = 'Hello World!3';
+  public message: string = 'Hello World!4';
   public result : string = "";
   public cards: Card[] = [];
   public query: string = "";
